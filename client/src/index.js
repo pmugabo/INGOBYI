d585ba0665c65ca271c510af944e69101ba4b888
Mugabo1 <154597425+Mugabo1@users.noreply.github.com>
2025-03-25 07:26:06 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
